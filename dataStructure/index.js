c3272c0286f52d49123f945756dc1ffc6b6e9422
Codebybhuvi <codebybhuvi@gmail.com>
2025-07-08 08:27:09 +0000

    array

---
+++ dataStructure/index.js
@@ -0,0 +1,7 @@
+// thes is all about arrays
+
+
+// how to declared array, javascritp me array ke andar kuchh bhi store kar sakte hai jaise ki object stirng funcition and nested arrray everthing store inside array if you want 
+
+const arr =[1,2,3,4,5,6]
+console.log(arr)
