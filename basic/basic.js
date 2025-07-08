d85927de3ea8b947dd46fae8f6a918e5f73288d9
Codebybhuvi <codebybhuvi@gmail.com>
2025-07-08 08:03:03 +0000

    operations

--- basic/basic.js
+++ basic/basic.js
@@ -1,5 +1,38 @@
-console.log("hello")
-let a = 5
-let b = 9
-let c = a+b
-console.log(c)
+// console.log("hello")
+// let a = 5
+// let b = 9
+// let c = a+b
+// console.log(c)
+
+// // in js 3 types of variables 
+
+// let bhui = "bhuvi";
+// console.log(bhui)
+
+// var pi = 3.01
+
+// var aa = 123;
+// //before edit
+
+// console.log(bhui);
+// console.log(pi)
+// console.log(aa)
+
+// aa = 12;
+// pi = 5
+// bhui = "bhuvneshwar";
+// console.log(bhui);
+// console.log(pi)
+// console.log(aa)
+
+let name = "Bhuvneshwar patel"
+name = 55
+// console.log(typeof name)
+// console.log(typeof(name))// both syntax checking a type of  datatype
+
+let newname = String(name);
+console.log(typeof newname)
+console.log(newname)
+
+
+
